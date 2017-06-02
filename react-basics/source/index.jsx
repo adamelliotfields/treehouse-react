82b2fbb15e87fc81b36d540ba21086be68bdd61c
Adam Fields <adam.elliot.fields@gmail.com>
2017-06-02 17:33:07 -0400

    Project dependencies installed. Build and watch scripts working.

---
+++ react-basics/source/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './css/main.css';
+import Application from './components/Application.jsx';
+
+render(<Application title='Scoreboard' />, document.getElementById('root'));
