622a1894e86c96d46d163b13ada074ce792eb615
Adam Fields <adam.elliot.fields@gmail.com>
2017-06-15 23:44:43 -0400

    Added Webpack configs.

---
+++ react-data-fetching/webpack.config.dev.js
@@ -0,0 +1,47 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    './source/jsx/index.jsx'
+  ],
+  output: {
+    filename: 'bundle.js',
+    publicPath: '/js/'
+  },
+  devtool: 'eval',
+  module: {
+    rules: [
+      {
+        test: /\.css?$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
+        ]
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-3']
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('development')
+      }
+    })
+  ],
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    contentBase: ['source']
+  }
+};
