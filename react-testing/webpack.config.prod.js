74f3ac53c6fddb99fd1ea0b735f9eff9bf0bad11
Adam Fields <adam.elliot.fields@gmail.com>
2017-06-15 20:44:28 -0400

    Switched from Gulp + Browserify to Webpack.

---
+++ react-testing/webpack.config.prod.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const webpack = require('webpack');
+const cssnano = require('cssnano');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: [
+    './source/jsx/index.jsx'
+  ],
+  output: {
+    filename: './js/bundle.js',
+    path: path.join(__dirname, 'build')
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css?$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              plugins: [
+                cssnano({
+                  discardComments: {
+                    removeAll: true
+                  }
+                })
+              ]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-3']
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      comments: false,
+      sourceMap: true
+    }),
+    new CopyWebpackPlugin([
+      { from: 'source/index.html', to: './index.html' },
+      { from: 'source/favicon.ico', to: './favicon.ico' },
+      { from: 'source/apple-touch-icon.png', to: './apple-touch-icon.png' },
+      { from: 'source/images', to: './images' }
+    ])
+  ]
+};
